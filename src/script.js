2e8ac5bdb17dffa064190720b4b9852280af4257
Caio Diniz <caiodinix@gmail.com>
2024-02-12 17:11:50 -0300

    melhorias no codigo

--- src/script.js
+++ src/script.js
@@ -1,11 +1,37 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function Calculadora(numero1, numero2, operador) {
+  let num1 = Number(numero1);
+  let num2 = Number(numero2);
+  let resultado;
+  let contador = 1
 
-//ESCREVA SEU CÓDIGO AQUI!!
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  switch (operador) {
+    case "+":
+      resultado = num1 + num2
+      break;
+    case "-":
+      resultado = num1 - num2
+      break;
+    case "*":
+      resultado = num1 * num2
+      break;
+    case "/":
+      resultado = num1 / num2
+      break;
+    case "e":
+      resultado = num1
+      if (num2 == 0) {
+        resultado = 1
+      } else {
+          for (contador; contador < num2; contador ++) {
+            resultado *= num1
+          }
+        }
+  }
+  if  (resultado == undefined || resultado > 1000000) {
+    resultado = "ERRO";
+  }
+  return resultado;
+}
 
 let visor = document.getElementById("visor");
 
